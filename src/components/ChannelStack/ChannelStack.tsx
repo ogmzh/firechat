4a14a6b949deb65d4824c815d20a611d3e43c282
Ognjen Mišić <omg.gio@gmail.com>
2022-06-27 19:18:27 +0200

    display member of channels if applicable

--- src/components/ChannelStack/ChannelStack.tsx
+++ src/components/ChannelStack/ChannelStack.tsx
@@ -1,35 +1,33 @@
 import { Divider, Stack, StackProps, Text } from '@mantine/core';
-import { useDidUpdate } from '@mantine/hooks';
-import { atom, useAtom } from 'jotai';
-import { isEqual } from 'lodash-es';
+import { atom } from 'jotai';
+import useCommunalChannels from '../../services/firebase/useCommunalChannels';
 import useOwnChannels from '../../services/firebase/useOwnChannels';
 import { ChannelEntity } from '../../shared/Types';
 import ChannelLink from '../ChannelLink/ChannelLink';
 
 export const selectedChannelAtom = atom<ChannelEntity | null>(null);
-export const ownedChannelsAtom = atom<ChannelEntity[]>([]);
 
 export default function ChannelStack(props: StackProps) {
-  const [ownedChannels, setOwnedChannels] = useAtom(ownedChannelsAtom);
-
-  const { channels } = useOwnChannels();
-
-  useDidUpdate(() => {
-    if (!!channels && !isEqual(channels, ownedChannels)) {
-      setOwnedChannels(channels);
-    }
-  }, [channels]);
+  const { channels: ownedChannels } = useOwnChannels();
+  const { channels: communalChannels } = useCommunalChannels();
 
   return (
-    <Stack {...props}>
-      <Text size="sm" hidden={ownedChannels.length === 0}>
-        Owner of
-      </Text>
-      {ownedChannels?.map(channel => (
-        <ChannelLink key={channel.id} channel={channel} />
-      ))}
-      <Divider my="sm" />
-      <Text size="sm">Member of</Text>
-    </Stack>
+    <>
+      <Stack {...props}>
+        <Text size="sm" hidden={!!ownedChannels ? ownedChannels.length === 0 : true}>
+          Owner of
+        </Text>
+        {ownedChannels?.map(channel => (
+          <ChannelLink key={channel.id} channel={channel} />
+        ))}
+      </Stack>
+      <Stack hidden={!!communalChannels ? communalChannels.length === 0 : true} mt="md">
+        <Divider my="sm" />
+        <Text size="sm">Member of</Text>
+        {communalChannels?.map(channel => (
+          <ChannelLink key={channel.id} channel={channel} />
+        ))}
+      </Stack>
+    </>
   );
 }
